f6c0e0a474ed080a566deaa31f5bb4253c11aed5
gonzalo.diez <gonzalodiez97@gmail.com>
2022-08-12 15:22:50 -0300

    Proyecto 1

--- Proyecto/Proyecto/ADO/UsuarioHandler.cs
+++ Proyecto/Proyecto/ADO/UsuarioHandler.cs
@@ -12,7 +12,7 @@ namespace Proyecto.ADO.NET
 {
     public class UsuarioHandler : DbHandler
     {
-        public List<Usuario> GetUsuarios(int id)
+        public List<Usuario> GetUsuarios()
         {
             List<Usuario> usuarios = new List<Usuario>();
             using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
