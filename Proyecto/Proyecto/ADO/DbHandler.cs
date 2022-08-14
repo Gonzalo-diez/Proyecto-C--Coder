3271ae39c5f4c88f5fa4a1b3bc87c4c6ca0961f0
gonzalo.diez <gonzalodiez97@gmail.com>
2022-08-14 13:43:47 -0300

    Cambios

--- Proyecto/Proyecto/ADO/DbHandler.cs
+++ Proyecto/Proyecto/ADO/DbHandler.cs
@@ -2,6 +2,6 @@
 {
     public abstract class DbHandler
     {
-        public const string ConnectionString = "Server=192.168.1.4;Initial Catalog=SistemaGestion;Trusted_Conection=true;UserID=UsuarioRemoto;Password=Socrates2001;";
+        public const string ConnectionString = "Server=192.168.1.4;Initial Catalog=SistemaGestion;Trusted_Conection=true;";
     }
 }
